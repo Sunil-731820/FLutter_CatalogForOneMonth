8b2c32b281d945d794538cd575ec82bad8ac65f3
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2022-05-17 17:31:32 +0530

    coded

--- days2/lib/home_page.dart
+++ days2/lib/home_page.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "GFG";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ,
+     appBar: AppBar(
+       title: Text("days 2 parts of flutter"),
+     ),
+     body: Center(
+       child: Container(
+         child: Text(
+           "welcome to my flutter Blog $days and Flutter website is $name"
+         ),
+       ),
+     ),
     );
-
   }
 }
