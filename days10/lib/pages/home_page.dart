d305d201267a6b4e7c2703ee0dc9edd913d912cd
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-05-29 10:30:26 +0530

    coded

---
+++ days10/lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/drawer.dart';
+
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Sunil";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: MyDrawer(),
+    );
+  }
+}
