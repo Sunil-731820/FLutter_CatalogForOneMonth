0282814059bd3fa2e9801d127055d461dc2c0c22
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-07-26 10:50:10 +0530

    coded

--- widgetsInFlutter/container_in_flutter/lib/main.dart
+++ widgetsInFlutter/container_in_flutter/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 void main(List<String> args) {
   runApp(MyApp());
 }
@@ -10,22 +9,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Container ",
+      title: "Container",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Welcome To App",
-          style: TextStyle(fontSize: 12),
+          title: Text("Container",
+          style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
         ),
+        body: Container(
+          width: double.infinity,
+          height: 200,
+          color: Colors.purple,
+          child: Container(
+            width: 50,
+            height: 100,
+            color: Colors.redAccent,
+            child: Center(
+              child: Text("This is the container ",
+              style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+            ),
+          ),
+        ),
+        
       ),
-      body: Container(
-        width :double.infinity,
-        height: 100,
-        color: Colors.purple,
-        child: Text("Welcome to Container ",
-        style: TextStyle(fontSize: 20),),
-      ),
-    ),
-    
+      
+      
     );
+    
   }
 }
